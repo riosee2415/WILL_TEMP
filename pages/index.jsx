6d961f640be9956331f953ffd54f7157b8557a5d
4leaf.ysh <riosee2415@gmail.com>
2023-07-20 16:17:00 +0900

    a

--- pages/index.jsx
+++ pages/index.jsx
@@ -3,7 +3,11 @@ import React from "react";
 const Index = () => {
   return (
     <div>
-      <h1>Hello World</h1>
+      <img
+        style={{ width: "100%", height: "100vh", objectFit: "cover" }}
+        src="https://4leaf-s3.s3.ap-northeast-2.amazonaws.com/willmarkets/assets/images/banner/main.png"
+        alt="information"
+      />
     </div>
   );
 };
